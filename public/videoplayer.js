c1ebd423b4212cfb7e29b2e403bd6373ea0b44f1
Esben Holk <esbenholknielsen@gmail.com>
2020-03-18 17:50:11 +0100

    delted copy: starting from fresh

--- public/videoplayer.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("videoplayer").innerHTML =
-    "<iframe title='YouTube video player'   type='text/html' width='640' height='390' src='https://www.youtube.com/watch?v=95dOqEgFdmk&t=424s' frameborder='0' allowFullScreen></iframe>";
